9bfcae96cbdc3aacc503c9ad94295eaf229f2685
Rana Awais <ranaawais3553@gmail.com>
2020-09-21 01:09:35 +0500

    Updates

---
+++ precache-manifest.81698652b5b34958ff186f6700793507.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60673e5132d32f37b24797fd1db3e4e",
+    "url": "/ReactifolioBs4_Akj/index.html"
+  },
+  {
+    "revision": "11798039084bf5159f2b",
+    "url": "/ReactifolioBs4_Akj/static/css/2.c3594674.chunk.css"
+  },
+  {
+    "revision": "ed5d4d66d306c65af917",
+    "url": "/ReactifolioBs4_Akj/static/css/main.97230796.chunk.css"
+  },
+  {
+    "revision": "11798039084bf5159f2b",
+    "url": "/ReactifolioBs4_Akj/static/js/2.0caabfb8.chunk.js"
+  },
+  {
+    "revision": "45614e2db553ddbebefb453383ce98e9",
+    "url": "/ReactifolioBs4_Akj/static/js/2.0caabfb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed5d4d66d306c65af917",
+    "url": "/ReactifolioBs4_Akj/static/js/main.b0dd2efe.chunk.js"
+  },
+  {
+    "revision": "e1b81b1a85e4212fc570",
+    "url": "/ReactifolioBs4_Akj/static/js/runtime-main.4e11bf4a.js"
+  },
+  {
+    "revision": "21999f09aca6272cd15a16d949434923",
+    "url": "/ReactifolioBs4_Akj/static/media/accenture-3.21999f09.svg"
+  },
+  {
+    "revision": "dec8d9fd61867920e33f5e75e6aa8aed",
+    "url": "/ReactifolioBs4_Akj/static/media/awaispic.dec8d9fd.jpg"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/ReactifolioBs4_Akj/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ac647b5872bca7424da864d11a3c5c40",
+    "url": "/ReactifolioBs4_Akj/static/media/backslide1.ac647b58.jpg"
+  },
+  {
+    "revision": "c8748c4ec97d9ef4d99c4de0760886af",
+    "url": "/ReactifolioBs4_Akj/static/media/backslide2.c8748c4e.jpg"
+  },
+  {
+    "revision": "453b974ba4828e5e80ba83d1a3832847",
+    "url": "/ReactifolioBs4_Akj/static/media/backslide3.453b974b.jpg"
+  },
+  {
+    "revision": "8d190f023135b6c392be895e929c5b0b",
+    "url": "/ReactifolioBs4_Akj/static/media/classroom.8d190f02.jpg"
+  },
+  {
+    "revision": "328886543ab20825f1707bf5647d1a47",
+    "url": "/ReactifolioBs4_Akj/static/media/logo.32888654.png"
+  },
+  {
+    "revision": "210d83de03dbbcf3c1a8689dbbb2b5b1",
+    "url": "/ReactifolioBs4_Akj/static/media/profile.210d83de.webp"
+  },
+  {
+    "revision": "9a20fffc18992171ecdbe686ac53aa51",
+    "url": "/ReactifolioBs4_Akj/static/media/school1.9a20fffc.jpeg"
+  },
+  {
+    "revision": "7ffe0901da416e3e076344e1bed48fe8",
+    "url": "/ReactifolioBs4_Akj/static/media/school2.7ffe0901.png"
+  },
+  {
+    "revision": "965c6c08dab55ce9c2872e72bcb27eca",
+    "url": "/ReactifolioBs4_Akj/static/media/staff1.965c6c08.jpg"
+  }
+]);
